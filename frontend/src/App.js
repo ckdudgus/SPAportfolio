6ffa6609162ae536564213482c3ffcd5f4209485
ckdudgus <c91878074@gmail.com>
2022-04-18 11:05:45 +0900

    리스트호출실행

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import InterviewInsert from './component/InterviewInsert';
+
+
+function App() {
+  return (
+    <div className="Wrap">
+      <InterviewInsert dbinfo={ {         
+         titlenm : '아마존 폼태그', 
+         botable : 'interviewwrite',
+         crud : 'insert',
+         mapper : 'introduceSQL',
+         mapperid : 'interviewInsert'
+         }
+        }></InterviewInsert>
+
+    </div>
+  );
+}
